b6f081ca376c2528a8aaa86cb8b8a0e5e450e206
410 <58301450+410-dev@users.noreply.github.com>
2023-01-14 21:46:03 -0500

    Update Comment.java

--- src/main/java/org/mcmasterkboys/codenamehenryford/objects/posts/Comment.java
+++ src/main/java/org/mcmasterkboys/codenamehenryford/objects/posts/Comment.java
@@ -1,8 +1,12 @@
 package org.mcmasterkboys.codenamehenryford.objects.posts;
 
+import lombok.Getter;
+import lombok.Setter;
 import me.hy.libhyextended.objects.DatabaseObject;
 import org.mcmasterkboys.codenamehenryford.modules.SQLConnectionFactory;
 
+@Getter
+@Setter
 public class Comment extends DatabaseObject {
 
     private String uuid;
@@ -18,8 +22,19 @@ public class Comment extends DatabaseObject {
     private String pkName = "uuid";
     private String pkType = "String";
 
-
     public Comment() {
         super(SQLConnectionFactory.class);
+    }
+
+    public Comment(String uuid, String content, String authorUUID, String authorName, String postUUID, long postTime, int likes, int dislikes) {
+        super(SQLConnectionFactory.class);
+        this.uuid = uuid;
+        this.content = content;
+        this.authorUUID = authorUUID;
+        this.authorName = authorName;
+        this.postUUID = postUUID;
+        this.postTime = postTime;
+        this.likes = likes;
+        this.dislikes = dislikes;
     }
 }
