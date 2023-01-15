31e1b045d56e6380d5c3896c9a45744c57b99dc2
410 <58301450+410-dev@users.noreply.github.com>
2023-01-14 21:42:09 -0500

    Update Post.java

--- src/main/java/org/mcmasterkboys/codenamehenryford/objects/posts/Post.java
+++ src/main/java/org/mcmasterkboys/codenamehenryford/objects/posts/Post.java
@@ -1,20 +1,40 @@
 package org.mcmasterkboys.codenamehenryford.objects.posts;
 
+import lombok.Getter;
 import me.hy.libhyextended.objects.DatabaseObject;
-import org.mcmasterkboys.codenamehenryford.objects.User;
+import org.mcmasterkboys.codenamehenryford.modules.SQLConnectionFactory;
 
+@Getter
 public class Post extends DatabaseObject {
 
     private String uuid;
     private String title;
     private String content;
-    private User author;
+    private String authorUUID;
+    private String authorName;
+    private String boardUUID;
+    private long postTime;
+    private int likes;
+    private int dislikes;
 
     private String tableName = "posts";
     private String pkName = "uuid";
     private String pkType = "String";
 
-    public Post(Class<?> sqlConnectionFactoryClass) {
-        super(sqlConnectionFactoryClass);
+    public Post() {
+        super(SQLConnectionFactory.class);
+    }
+
+    public Post(String uuid, String title, String content, String authorUUID, String authorName, String boardUUID, long postTime, int likes, int dislikes) {
+        super(SQLConnectionFactory.class);
+        this.uuid = uuid;
+        this.title = title;
+        this.content = content;
+        this.authorUUID = authorUUID;
+        this.authorName = authorName;
+        this.boardUUID = boardUUID;
+        this.postTime = postTime;
+        this.likes = likes;
+        this.dislikes = dislikes;
     }
 }
