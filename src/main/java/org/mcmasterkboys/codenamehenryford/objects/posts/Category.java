06ed4d081d5b124b362e84eb3b467fc0d64bd0aa
410 <58301450+410-dev@users.noreply.github.com>
2023-01-14 21:42:00 -0500

    Update Category.java

--- src/main/java/org/mcmasterkboys/codenamehenryford/objects/posts/Category.java
+++ src/main/java/org/mcmasterkboys/codenamehenryford/objects/posts/Category.java
@@ -1,14 +1,28 @@
 package org.mcmasterkboys.codenamehenryford.objects.posts;
 
 import me.hy.libhyextended.objects.DatabaseObject;
+import org.mcmasterkboys.codenamehenryford.modules.SQLConnectionFactory;
 
 public class Category extends DatabaseObject {
 
-    private String tableName = "posts";
+    private String uuid;
+    private String name;
+    private String description;
+    private String iconPath;
+
+    private String tableName = "category";
     private String pkName = "uuid";
     private String pkType = "String";
 
-    public Category(Class<?> sqlConnectionFactoryClass) {
-        super(sqlConnectionFactoryClass);
+    public Category() {
+        super(SQLConnectionFactory.class);
+    }
+
+    public Category(String uuid, String name, String description, String iconPath) {
+        super(SQLConnectionFactory.class);
+        this.uuid = uuid;
+        this.name = name;
+        this.description = description;
+        this.iconPath = iconPath;
     }
 }
