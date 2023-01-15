d6722e00ab7f6ed927d849460cd84bcef905f963
410 <58301450+410-dev@users.noreply.github.com>
2023-01-14 21:20:40 -0500

    Update Address.java

--- src/main/java/org/mcmasterkboys/codenamehenryford/objects/Address.java
+++ src/main/java/org/mcmasterkboys/codenamehenryford/objects/Address.java
@@ -2,19 +2,18 @@ package org.mcmasterkboys.codenamehenryford.objects;
 
 import lombok.Getter;
 import lombok.Setter;
+import me.hy.libhyextended.objects.DataObject;
 
 @Getter
 @Setter
-public class Address extends DataObject{
+public class Address extends DataObject {
     private String street;
     private String city;
     private String province;
     private String country;
     private String postalCode;
 
-
     public Address(){}
-
     public Address(String street, String city, String province, String country, String postalCode) {
         this.street = street;
         this.city = city;
