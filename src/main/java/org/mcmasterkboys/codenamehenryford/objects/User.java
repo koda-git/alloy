56b3b7f16879817cd8fc2e6117b4926dc038ea49
410 <58301450+410-dev@users.noreply.github.com>
2023-01-14 22:43:42 -0500

    Update User.java

--- src/main/java/org/mcmasterkboys/codenamehenryford/objects/User.java
+++ src/main/java/org/mcmasterkboys/codenamehenryford/objects/User.java
@@ -2,8 +2,15 @@ package org.mcmasterkboys.codenamehenryford.objects;
 
 import lombok.Getter;
 import lombok.Setter;
+import me.hy.libhycore.CoreSHA;
+import me.hy.libhyextended.mail.HEMail;
+import me.hy.libhyextended.mail.servers.HEGmail;
 import me.hy.libhyextended.objects.DatabaseObject;
+import me.hy.libhyextended.objects.exception.DataFieldMismatchException;
+import me.hy.libhyextended.objects.exception.UndefinedSQLKeyException;
 import org.mcmasterkboys.codenamehenryford.modules.SQLConnectionFactory;
+
+import java.sql.SQLException;
 
 
 @Getter
@@ -17,6 +24,8 @@ public class User extends DatabaseObject {
     private String uuid;
     private String phoneNumber;
     private String gender;
+    private String verificationCode;
+    private boolean verified;
     private Address address;
 
     private String tableName = "users";
@@ -27,7 +36,7 @@ public class User extends DatabaseObject {
         super(SQLConnectionFactory.class);
     }
 
-    public User(String uuid, String lastName, String firstName, String email, String password, String phoneNumber, Gender gender, Address address) {
+    public User(String uuid, String lastName, String firstName, String email, String password, String phoneNumber, String gender, String verificationCode, boolean verified, Address address) {
         super(SQLConnectionFactory.class);
         this.lastName = lastName;
         this.firstName = firstName;
@@ -35,8 +44,32 @@ public class User extends DatabaseObject {
         this.password = password;
         this.uuid = uuid;
         this.phoneNumber = phoneNumber;
-        this.gender = gender.toString();
+        this.gender = gender;
+        this.verificationCode = verificationCode;
+        this.verified = verified;
         this.address = address;
     }
 
+    public void loadUsingEmail(String email) throws SQLException, DataFieldMismatchException {
+        String origPkName = pkName;
+        String origPkType = pkType;
+        String origPkVal = this.getPkValue();
+
+        pkName = "email";
+        pkType = "String";
+        this.setPkValue(email);
+        this.select();
+
+        this.pkName = origPkName;
+        this.pkType = origPkType;
+        this.setPkValue(origPkVal);
+    }
+
+    public String hashPassword(String password) {
+        return CoreSHA.hash512(password, "cc519fa2f4a333a8b4ce6021041fd3b5057203d9fa057aa3e9a263cc4cb9d8b678b58180890bc181a6bed1fa34d5b593fd2dd15cb37dff17ed3370958846f158");
+    }
+
+    public void sendVerificationCode() {
+        // TODO: Send verification code to user's email
+    }
 }
