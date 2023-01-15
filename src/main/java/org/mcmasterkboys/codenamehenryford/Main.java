9a952415adbadc0fbb7b91210378eefd1d7e4f26
410 <58301450+410-dev@users.noreply.github.com>
2023-01-14 19:01:37 -0500

    Update Main.java

--- src/main/java/org/mcmasterkboys/codenamehenryford/Main.java
+++ src/main/java/org/mcmasterkboys/codenamehenryford/Main.java
@@ -7,20 +7,5 @@ import org.mcmasterkboys.codenamehenryford.objects.exception.DataFieldMismatchEx
 public class Main {
     public static void main(String[] args) throws DataFieldMismatchException {
 
-        Address a = new Address("1234", "Toronto", "ON", "Canada", "M4T 1J8");
-        //public User(String lastName, String firstName, String email, String password, String uuid, String phoneNumber, Address address) {
-        User u = new User("Appleseed", "John", "johnappleseed@apple.com", "password", "1234", "1234567890", a);
-
-        System.out.println(u.toJson());
-
-        System.out.println("!!!!!!!!Conversion done!!!!!!!!!!!");
-
-        User o = new User();
-        o.fromJson(u.toJson(), Address.class);
-
-        System.out.println("!!!!!!!!Parsing done!!!!!!!!!!!");
-
-        System.out.println(o.toJson());
-
     }
 }
